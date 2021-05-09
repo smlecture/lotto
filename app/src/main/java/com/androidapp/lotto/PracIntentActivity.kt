63552632904d2e515ac7cd84533c574068d1649d
H-J Yoo <yoohj@smu.ac.kr>
2021-05-09 15:15:43 +0900

    Main activity with Linear Layout

---
+++ app/src/main/java/com/androidapp/lotto/PracIntentActivity.kt
@@ -0,0 +1,11 @@
+package com.androidapp.lotto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PracIntentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_prac_intent)
+    }
+}
